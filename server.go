fa0ff82290c8ad7da882b54843e2a1a9dcc351e3
mell <chanon4817@gmail.com>
2023-01-04 15:04:52 +0700

    :sparkles: feat(api): create, getAll handler
    some issues as repo connect to db

--- server.go
+++ server.go
@@ -52,7 +52,7 @@ func InitDB() {
 		title TEXT, 
 		amount FLOAT, 
 		note TEXT, 
-		tags VARCHAR
+		tags TEXT[]
 		);`
 	_, err = db.Exec(createTable)
 	if err != nil {
@@ -60,28 +60,85 @@ func InitDB() {
 	}
 }
 
+// Repository
+func GetExpenses(db *sql.DB) ([]Expense, error) {
+	// var expenses = []Expense{}
+	expenses := []Expense{}
+	statement, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses")
+	if err != nil {
+		// return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expense statement"})
+		return expenses, err
+	}
+	rows, err := statement.Query()
+	if err != nil {
+		// return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expense statement"})
+		return expenses, err
+	}
+
+	for rows.Next() {
+		var e Expense
+		err = rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, &e.Tags)
+		if err != nil {
+			// return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expenses - " + err.Error()})
+			return expenses, err
+		}
+		expenses = append(expenses, e)
+	}
+	// return c.JSON(http.StatusOK, expenses)
+	return expenses, nil
+}
+
+func CreateExpenses(db *sql.DB, e *Expense) error {
+	// func CreateExpensesHandler(db *sql.DB) error {
+	// var e Expense
+	// err := c.Bind(&e)
+	// if err != nil {
+	// 	return c.JSON(http.StatusBadRequest, Err{Message: "can't bind to Expense - " + err.Error()})
+	// }
+
+	rows := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values ($1,$2,$3,$4) RETURNING id", &e.Title, &e.Amount, &e.Note, &e.Tags)
+	err := rows.Scan(&e.ID)
+	if err != nil {
+		// return c.JSON(http.StatusBadRequest, Err{Message: "can't insert into expenses - " + err.Error()})
+		return err
+	}
+	// return c.JSON(http.StatusCreated, "OK")
+	return nil
+}
+
 // ดึงข้อมูลการใช้จ่ายทั้งหมด
 func GetExpensesHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, "OK")
-
+	expenses, err := GetExpenses(db)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expense statement" + err.Error()})
+	}
+	return c.JSON(http.StatusOK, expenses)
 }
 
 // ดึงข้อมูลการใช้จ่ายทีละรายการ
 func GetExpensesIdHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, "OK")
-
 }
 
 // เพิ่มประวัติการใช้จ่ายใหม่ได้
 func CreateExpensesHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, "OK")
+	var e Expense
+	err := c.Bind(&e)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: "can't bind to Expense - " + err.Error()})
+	}
 
+	// err = rows.Scan(&e.ID)
+	err = CreateExpenses(db, &e)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: "can't insert into expenses - " + err.Error()})
+	}
+	return c.JSON(http.StatusCreated, "OK")
 }
 
 // ปรับเปลี่ยน/แก้ไข ข้อมูลของการใช้จ่ายได้
 func UpdateExpensesHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, "OK")
-
 }
 
 func healthHandler(c echo.Context) error {
@@ -95,10 +152,6 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// Init handler
-	// h := CustomerHandler{}
-	// h.Initialize()
 
 	// Routing
 	e.GET("/expenses", GetExpensesHandler)
